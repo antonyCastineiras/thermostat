06b48c900cf2c2e5c67e7eb4246ea50f0e887e98
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-28 12:32:58 +0100

    added a post route

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
 require_relative 'models/thermostat_state'
+require 'open-uri'
+require 'json'
 
 class Thermostat < Sinatra::Base
   get '/' do
     erb(:index)
+  end
+
+  post '/thermostat/update' do
+  	request.body.rewind
+  	data = JSON.parse(request.body.read)
+  	"the data is: #{request.body}"
   end
 
   # start the server if ruby file executed directly
--- app/models/thermostat_state.rb
+++ app/models/thermostat_state.rb
@@ -8,6 +8,6 @@ class ThermostatState
   property :temperature, Integer
 end
 
-DataMapper.setup(:default, ENV['DATABSE_URL'] || "postgres://localhost/thermostat_development}"))
+DataMapper.setup(:default, ENV['DATABSE_URL'] || "postgres://localhost/thermostat_development")
 DataMapper.finalize
 DataMapper.auto_upgrade!
